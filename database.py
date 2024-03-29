3f43e230284f9950202bac84d8a47c09ed6c161e
rachid-buzurtanov <146711214+rachid-buzurtanov@users.noreply.github.com>
2024-03-29 14:42:16 +0100

    Ajout de la méthode check_user qui enverra une requête à notre base de données. Son objectif est de vérifier que l'utilisateur a un compte existant

--- database.py
+++ database.py
@@ -9,3 +9,20 @@ def __init__(self, host, user, password, database):
         database="myDiscord"
     )
     self.curseur = self.connexion.cursor()
+
+def check_user(username, password):
+    connexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="za9?-U5zwD4-6#L",
+        database="myDiscord"
+    )
+    curseur = connexion.cursor()
+
+    requete = "SELECT * FROM identifiant WHERE pseudo = %s AND mots_de_passe = %s"
+    valeurs = (username, password)
+    curseur.execute(requete, valeurs)
+    user = curseur.fetchone()
+    curseur.close()
+    connexion.close()
+    return user
