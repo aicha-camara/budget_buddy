2c2d4b58cf9fdfc483749121aaaf1c973d1bd990
rachid-buzurtanov <146711214+rachid-buzurtanov@users.noreply.github.com>
2024-03-29 14:37:40 +0100

    Création d' un fichier database qui contiendra toutes les méthodes pour faire des requête a la base de donnée

---
+++ database.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+
+def __init__(self, host, user, password, database):
+    self.connexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="za9?-U5zwD4-6#L",
+        database="myDiscord"
+    )
+    self.curseur = self.connexion.cursor()
