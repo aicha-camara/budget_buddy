f5b0d91c15b92bd4f39d0d2b5605022cb6ec3e05
camara_aicha <aicha.ca@outlook.fr>
2024-04-03 11:45:03 +0200

    ajout des commentaires

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-from sign import Frames
-import tkinter as tk
+from sign import Frames  # Importation de la classe Frames depuis le module sign
+import tkinter as tk  # Importation de tkinter en tant que tk
 
 
 def main():
-    # Initialize main window
+    # Initialisation de la fenêtre principale
     root = tk.Tk()
-    root.title("Banque")
+    root.title("Budget")  # Définition du titre de la fenêtre
 
-    # Get screen size
+    # Obtenir la taille de l'écran
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
 
-    # Set main window size to half of the screen size
+    # Définir la taille de la fenêtre principale à la moitié de la taille de l'écran
     root.geometry(f"{screen_width // 2}x{screen_height // 2}")
 
-    # Initialize frames
+    # Initialisation des cadres
     frames = Frames(root)
 
-    # Display welcome frame
+    # Afficher le cadre de bienvenue
     frames.show_frame(frames.welcome_frame)
 
-    # Start application loop
+    # Démarrer la boucle d'application
     root.mainloop()
 
 
-# Program entry point
+# Point d'entrée du programme
 if __name__ == "__main__":
     main()
